4fa953ba8cb50442938dfd4b6073c0e492a71afa
beau currier <beaucurrier01@gmail.com>
2024-10-21 16:26:25 -0700

    updated the page.tsx in app

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='min-h-screen text-black flex flex-col items-center justify-center bg-gray-100 p-6'>
       <h1 className='text-4xl font-bold mb-8'>Weather App</h1>
-      <AutocompleteSearch />
+      <AutocompleteSearch mode ='home'/>
     </div>
   );
 }

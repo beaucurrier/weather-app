faa2545ab60f53613901158ee8a1c7c9b1652e69
beau currier <beaucurrier01@gmail.com>
2024-10-21 19:41:10 -0700

    added a secret to auth.tsx

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -4,13 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   pages: {
     signIn: "/signin",
   },
+  secret: process.env.NEXT_PUBLIC_SECRET
 };
 
 export default authOptions;

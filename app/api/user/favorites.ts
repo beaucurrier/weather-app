eaf9f0a9fd7950974189a09f3aab70b10c57455e
beau currier <beaucurrier01@gmail.com>
2024-10-22 19:29:56 -0700

    added a new if to favorites

--- app/api/user/favorites.ts
+++ app/api/user/favorites.ts
@@ -7,10 +7,11 @@ import { getSession } from 'next-auth/react';
 export default async function getFavorites(req: NextApiRequest, res: NextApiResponse) {
   await dbConnect();
   const session = await getSession({ req });
+  console.log(session);
   if (!session) return res.status(401).send('Unauthorized');
 
   const user = await User.findOne({ email: session.user?.email }).populate('favoriteCities');
-  if (!user) return res.status(404).send('User not found');
-
+  if(!user || !user.favoriteCities) {return res.status(200).json([])}
+  
   res.status(200).json(user.favoriteCities);
 }

b6fc43e40abc510878d8d49392103c211aa54c4b
beau currier <beaucurrier01@gmail.com>
2024-10-28 17:27:39 -0700

    checking our errors

--- app/api/user/favorites/route.ts
+++ app/api/user/favorites/route.ts
@@ -4,8 +4,7 @@ import dbConnect from "../../../../lib/mongoose";
 import User, { IUser } from "../../../../models/User";
 import { getServerSession } from "next-auth";
 import authOptions from "../../../../lib/auth";
-// @ts-ignore
-import City, { ICity } from "../../../../models/City";
+import { ICity } from "../../../../models/City";
 
 export async function GET(): Promise<NextResponse> {
   await dbConnect();
